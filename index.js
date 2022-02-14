82ea4ede66f31316c4ceaa57f4bc9606dda69dec
Sivani Saravanan <rednaz.inavis@gmail.com>
2022-02-14 23:33:34 +0530

    cors

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ export const client = await createConnection();
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods","POST, PUT, GET, OPTIONS" )
     next();
   });
 app.use(express.json())
