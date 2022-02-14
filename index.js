f3c301856d708a9c8f7277ce3741b2aeda1ba6b2
Sivani Saravanan <rednaz.inavis@gmail.com>
2022-02-14 23:08:01 +0530

    cors

--- index.js
+++ index.js
@@ -22,7 +22,12 @@ export const client = await createConnection();
 
 //middleware
 app.use(express.json())
-app.use(cors())
+app.use(cors(
+    {
+        origin:"http://localhost:3000/",
+        methods:["GET","POST","PUT","DELETE"]
+    }
+))
 
 //routes
 app.use("/receipe",receipeRouter);
