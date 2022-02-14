a718bd934a5de6dbe2525c2503a1a9adab2e5b3f
Sivani Saravanan <rednaz.inavis@gmail.com>
2022-02-14 23:19:34 +0530

    cors

--- index.js
+++ index.js
@@ -24,8 +24,7 @@ export const client = await createConnection();
 app.use(express.json())
 app.use(cors(
     {
-        origin:"*",
-        methods:["GET","POST","PUT","DELETE"]
+        origin:"http://localhost:3000"
     }
 ))
 
