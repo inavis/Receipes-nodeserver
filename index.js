bde62544522f3243feece64bb7aab00ccc742a90
Sivani Saravanan <rednaz.inavis@gmail.com>
2022-02-15 10:18:29 +0530

    co%rs

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ export const client = await createConnection();
 //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 //     next();
 //   });
+app.use(cors())
 app.use(express.json())
 // app.use(cors(
 //     {
@@ -37,16 +38,16 @@ app.use(express.json())
 //routes
 app.use("/receipe",receipeRouter);
 
-app.all('*', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    if ('OPTIONS' == req.method) {
-    res.sendStatus(200);
-    } else {
-      next();
-    }
-  });
+// app.all('*', function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type');
+//     if ('OPTIONS' == req.method) {
+//     res.sendStatus(200);
+//     } else {
+//       next();
+//     }
+//   });
 
 app.get("/",(request,response)=>{
     response.send("receipes API");
