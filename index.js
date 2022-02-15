a5ec4a4970d9b9f44999d1bc54496a2708152f19
Sivani Saravanan <rednaz.inavis@gmail.com>
2022-02-15 20:27:44 +0530

    cors

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.options('*', cors());
 
 app.all('*', (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", ["Content-Type", "Authorization", "Content-Length", "X-Requested-With"]);
+  res.header("Access-Control-Allow-Headers", ["Content-Type", "Authorization", "Content-Length", "X-Requested-With","Accept","Origin"]);
   res.header("Access-Control-Allow-Methods", ["GET","PUT","POST","DELETE","OPTIONS"]);
   next();
 });
