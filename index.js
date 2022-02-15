df8f7cc19e99f8486d39c655c4b03c4850baf497
Sivani Saravanan <rednaz.inavis@gmail.com>
2022-02-15 20:26:27 +0530

    cors

--- index.js
+++ index.js
@@ -34,7 +34,9 @@ export const client = await createConnection();
 app.options('*', cors());
 
 app.all('*', (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", ["Content-Type", "Authorization", "Content-Length", "X-Requested-With"]);
+  res.header("Access-Control-Allow-Methods", ["GET","PUT","POST","DELETE","OPTIONS"]);
   next();
 });
 
