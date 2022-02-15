f029f8849ac28db969c7f1913334e2955f9593af
Sivani Saravanan <rednaz.inavis@gmail.com>
2022-02-15 18:33:39 +0530

    cors

--- index.js
+++ index.js
@@ -21,19 +21,15 @@ async function createConnection(){
 export const client = await createConnection();
 
 //middleware
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//     next();
-//   });
-app.use(cors())
+app.use(cors(
+    {
+        origin:"http://localhost:3000",
+        methods:["PUT"]
+    }
+))
+// app.use(cors())
 app.use(express.json())
-// app.use(cors(
-//     {
-//         origin:"http://localhost:3000"
-//     }
-// ))
+
 
 //routes
 app.use("/receipe",receipeRouter);
