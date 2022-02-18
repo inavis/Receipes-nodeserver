4832ad86a411709536c7352f6eeea4d087ade10e
Sivani Saravanan <rednaz.inavis@gmail.com>
2022-02-18 23:49:29 +0530

    adding auth

--- routes/receipe.js
+++ routes/receipe.js
@@ -1,21 +1,22 @@
 import express from "express";
 import { getAllReceipes,addReceipe, getReceipeById, deleteReceipe, updateReceipeById } from "../helper.js";
+import { auth } from "../middleware/auth.js";
 
 
 const router = express.Router();
 
-router.get("/",async(request,response)=>{
+router.get("/",auth,async(request,response)=>{
     let result = await getAllReceipes();
     response.send(result);
 })
 
-router.post("/",async(request,response)=>{
+router.post("/",auth,async(request,response)=>{
     let receipe = request.body;
     let result = await addReceipe(receipe);
     response.send(result);
 })
 
-router.get("/:id",async(request,response)=>{
+router.get("/:id",auth,async(request,response)=>{
     let {id} = request.params;
     let result = await getReceipeById(id);
     response.send(result);
@@ -27,7 +28,7 @@ router.delete("/:id",async(request,response)=>{
     response.send(result);
 })
 
-router.put("/:id",async(request,response)=>{
+router.put("/:id",auth,async(request,response)=>{
     let {id} = request.params;
     const updatedreceipe = request.body;
     let result = await updateReceipeById(id,updatedreceipe);
